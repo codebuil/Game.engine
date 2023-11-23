f84f1b4582de0cd7b5d7ef4a42b1803f55bb0d6c
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-11-23 21:37:33 +0000

    Add files via upload

--- thered.py
+++ thered.py
@@ -1,12 +1,18 @@
 import threading
 import random 
 import time
-
+import datetime
 def game(i):
     n=random.randint(1,9)
     for m in range(n):
         print(f"player:{i},x pos={m}")
-        time.sleep(1)
+        agora = datetime.datetime.now()
+        data_horas = agora.strftime("%S")
+        data_hora = agora.strftime("%S")
+        while(data_horas==data_hora):
+            agora = datetime.datetime.now()
+            data_horas = agora.strftime("%S")
+    
     print(f"player:{i},is in is position")
 
 print("\x1bc\x1b[43;30m")
