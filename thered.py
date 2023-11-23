e5fe5c8fcd505a4b4999dc3dd0121b09160518b5
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-11-23 21:56:22 +0000

    Add files via upload

--- thered.py
+++ thered.py
@@ -2,27 +2,35 @@ import threading
 import random 
 import time
 import datetime
-def game(i):
-    n=random.randint(1,9)
-    for m in range(n):
-        print(f"player:{i},x pos={m}")
-        agora = datetime.datetime.now()
-        data_horas = agora.strftime("%S")
-        data_hora = agora.strftime("%S")
-        while(data_horas==data_hora):
+class player:
+    def __init__(self,n):
+        self.tt=threading.Thread(target=self.game(n))
+        self.tt.start()
+
+
+    def game(self,i):
+        n=random.randint(1,9)
+        for m in range(n):
+            print(f"player:{i},x pos={m}")
             agora = datetime.datetime.now()
             data_horas = agora.strftime("%S")
+            data_hora = agora.strftime("%S")
+            while(data_horas==data_hora):
+                agora = datetime.datetime.now()
+                data_horas = agora.strftime("%S")
     
-    print(f"player:{i},is in is position")
+        print(f"player:{i},is in is position")
+
+    def endgame(self):
+        self.tt.join()
 
 print("\x1bc\x1b[43;30m")
 players=3
 t=[]
 for n in range(players):
-    tt=threading.Thread(target=game(n))
-    tt.start()
+    tt=player(n)
     t=t+[tt]
 
 for n in range(players):
     tt=t.pop()
-    tt.join()
+    tt.endgame()
