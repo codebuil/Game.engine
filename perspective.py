f2d19ba2b3ddff97735771d125c1c6eec10d1404
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-11-25 11:39:12 +0000

    Add files via upload

---
+++ perspective.py
@@ -0,0 +1,26 @@
+import threading
+import random 
+import time
+import datetime
+class moves:
+    def __init__(self,n):
+            
+        if 0==0:
+            for nn in range(n):
+                self.locate(int(n//2-nn//2),nn,43,"z")
+                self.locate(int(n//2+nn//2),nn,43,"z")
+                self.locate(nn,1,43,"x>>")
+                self.locate(1,nn,43,"y")
+                self.timers(1)
+        
+    def timers(self,s):
+        time.sleep(s)
+    def locate(self,xx,yy,c,s):
+        print(f"\x1b[{yy};{xx}f\x1b[{c};30m{s}")    
+
+        
+    
+print("\x1bc\x1b[43;30m")
+movesn=17
+t=moves(movesn)
+
