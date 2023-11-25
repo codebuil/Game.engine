2964ad9986615d5b861869f72e92cc607a2189c7
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-11-25 15:06:35 +0000

    Add files via upload

---
+++ order.py
@@ -0,0 +1,17 @@
+import threading
+import random 
+import time
+import datetime
+class xyz:
+    def __init__(self):
+        self.zxy=[(7,9,0),(3,6,4),(1,1,1)]   
+        self.zxy.sort()
+        self.report()
+    def report(self):
+        print(self.zxy)
+        
+    
+print("\x1bc\x1b[43;30m")
+
+t=xyz()
+
