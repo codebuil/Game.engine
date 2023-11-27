8a3716ed6008e931a61eb05d73e631d3eef9788b
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-11-27 15:11:27 +0000

    Add files via upload

--- savelines3d.py
+++ savelines3d.py
@@ -9,7 +9,7 @@ for n in range(50):
     z1=random.randint(0,10)
     y1=random.randint(0,10)-5
     x1=random.randint(0,10)-5
-    f1.write(f"{z0},{y0},{x0},{z1},{y1},{x1}")
+    f1.write(f"{z0},{y0},{x0},{z1},{y1},{x1}\n")
     print(f"{z0},{y0},{x0},{z1},{y1},{x1}")
 f1.close() 
 
